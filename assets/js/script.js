5b0303c33dc8163df5200a87dad257c30cdd12d7
cerafinn <74612014+cerafinn@users.noreply.github.com>
2021-04-29 17:41:54 -0400

    trying to resolve carousel issue

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,14 +8,14 @@ $(document).ready(function() {
     {
       title: "We travel all over the US",
       subtitle: "Check out our schedule!",
-      img: img1,
+      img: "../img/food-table.jpg",
       btnText: "View Schedule",
       btnUrl: "schedule.html"
     },
     {
       title: "Our food is seriously the bomb!",
       subtitle: "What are you waiting for?",
-      img: img2,
+      img: "../img/grill.jpg",
       btnText: "Purchase Tickets",
       btnUrl: "tickets.html"
     },
