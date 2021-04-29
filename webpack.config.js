e1b74ddfb06b8a9a5bf34d418e107d7048765121
cerafinn <74612014+cerafinn@users.noreply.github.com>
2021-04-29 00:20:02 -0400

    working on optimizing images

--- webpack.config.js
+++ webpack.config.js
@@ -10,8 +10,31 @@ module.exports = {
     tickets: "./assets/js/tickets.js"
   },
   output: {
-    path: path.join(__dirname + "/dist"),
     filename: "[name].bundle.js",
+    path: __dirname + "/dist",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jpg$/i,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name(file) {
+                return "[path][name].[ext]"
+              },
+              publicPath: function(url) {
+                return url.replace("../", "/assets/")
+              }
+            }
+          },
+          {
+            loader: "image-webpack-loader"
+          }
+        ]
+      }
+    ]
   },
   plugins: [
     new webpack.ProvidePlugin({
